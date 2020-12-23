74c5940e0ffc024c97d283e998bab38679265b44
Anhilly <62306250+Anhilly@users.noreply.github.com>
2020-12-23 10:30:55 +0100

    Cleaned code, deleted comments

--- src/js/tanks/Player.js
+++ src/js/tanks/Player.js
@@ -20,46 +20,19 @@ export default class Player extends Tank {
     }
 
     moveUp(lastInput) {
-        //Turn
-        /*
-        if (lastInput != 'w' && lastInput != 's') {
-            let hOld = this.tank.height;
-            this.tank.height = this.tank.width;
-            this.tank.width = hOld;
-        } */
         this.tank.y -= this.tank.speed;
     }
 
     moveRight(lastInput) {
-        //Turn
-        /*
-        if (lastInput != 'a' && lastInput != 'd') {
-            let hOld = this.tank.height;
-            this.tank.height = this.tank.width;
-            this.tank.width = hOld;
-        } */
         this.tank.x += this.tank.speed;
     }
 
 
-    moveDown(lastInput) {
-        //Turn
-        /*
-        if (lastInput != 'w' && lastInput != 's') {
-            let hOld = this.tank.height;
-            this.tank.height = this.tank.width;
-            this.tank.width = hOld;
-        } */
+    moveDown(lastInput) {      
         this.tank.y += this.tank.speed;
     }
     moveLeft(lastInput) {
-        //Turn
-        /*
-        if (lastInput != 'a' && lastInput != 'd') {
-            let hOld = this.tank.height;
-            this.tank.height = this.tank.width;
-            this.tank.width = hOld;
-        } */
+        
         this.tank.x -= this.tank.speed;
     }
 
@@ -68,17 +41,15 @@ export default class Player extends Tank {
     }
 
     rotateTank() {
-        console.log("x: ", this.tank.x, "| y: ", this.tank.y);
         //Sucht uns die mitte von unserem Panzer
         let offsetX = this.tank.x + this.tank.width / 2;
         let offsetY = this.tank.y + this.tank.height / 2;
-        console.log(offsetX, offsetY)
         this.rotate = Math.atan2(this.mousePosition.x - offsetX, -(this.mousePosition.y - offsetY));
     } 
 
     setMousePosition(mousePosition) {
         this.mousePosition.x = mousePosition.x;
-        this.mousePosition.y = mousePosition.y;
+        this.mousePosition.y = mousePosition.y; 
     }
 
     draw(ctx) {
@@ -94,9 +65,6 @@ export default class Player extends Tank {
         ctx.rotate(this.rotate);
         //Drawing the canon on our translated ctx
         ctx.drawImage(this.image.canon, -this.tank.width / 2, -35);
-        /*
-        ctx.strokeRect(this.tank.x, this.tank.y, this.tank.width, this.tank.height);
-        ctx.strokeRect(this.canon.x, this.canon.y, this.canon.width, this.canon.height); */
         ctx.restore();
     }
 
